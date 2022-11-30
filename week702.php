2f16497db0ed0975244b1cb1da354f619a4d29b4
natsunan <natsunan270@gmail.com>
2022-11-30 17:18:01 +0700

    update

---
+++ week702.php
@@ -0,0 +1,19 @@
+<?php
+class Person{
+    public $name = "Elon Mask" , $age = 50;//property
+
+    function __construct($n, $a)
+    {
+        $this->name = $n;
+        $this->age =$a;
+    }
+    function show(){
+        print ($this->name . $this->age);
+    }
+}
+
+$obj1 = new Person("mark Zuckerberg",45);
+// $obj1->name = "mark Zuckerberg <br />";
+// $obj1->age = 45;
+$obj1->show();
+?>
